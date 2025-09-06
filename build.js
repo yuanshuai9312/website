d1c11a45d2306737643543b4b05c6ad3bd78975b
yuanshuai9312 <61941927+yuanshuai9312@users.noreply.github.com>
2025-09-06 22:48:14 +0900

    Refactor pages to use Nunjucks partials

---
+++ build.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+const nunjucks = require('nunjucks');
+
+const srcDir = path.join(__dirname, 'src');
+const pagesDir = path.join(srcDir, 'pages');
+
+nunjucks.configure(srcDir, { autoescape: false });
+
+fs.readdirSync(pagesDir)
+  .filter(f => f.endsWith('.njk'))
+  .forEach(file => {
+    const out = nunjucks.render(path.join('pages', file));
+    const outPath = path.join(__dirname, file.replace(/\.njk$/, '.html'));
+    fs.writeFileSync(outPath, out, 'utf8');
+  });
