46d067fe00764ea8da8df7ae32c8106940800394
yuanshuai9312 <61941927+yuanshuai9312@users.noreply.github.com>
2025-09-07 00:49:30 +0900

    Document build and ignore dist

--- build.js
+++ build.js
@@ -1,16 +1,26 @@
-const fs = require('fs');
+const fs = require('fs/promises');
 const path = require('path');
 const nunjucks = require('nunjucks');
 
 const srcDir = path.join(__dirname, 'src');
 const pagesDir = path.join(srcDir, 'pages');
+const distDir = path.join(__dirname, 'dist');
 
 nunjucks.configure(srcDir, { autoescape: false });
 
-fs.readdirSync(pagesDir)
-  .filter(f => f.endsWith('.njk'))
-  .forEach(file => {
-    const out = nunjucks.render(path.join('pages', file));
-    const outPath = path.join(__dirname, file.replace(/\.njk$/, '.html'));
-    fs.writeFileSync(outPath, out, 'utf8');
-  });
+async function build() {
+  await fs.mkdir(distDir, { recursive: true });
+  const files = await fs.readdir(pagesDir);
+  for (const file of files) {
+    if (file.endsWith('.njk')) {
+      const out = nunjucks.render(path.join('pages', file));
+      const outPath = path.join(distDir, file.replace(/\.njk$/, '.html'));
+      await fs.writeFile(outPath, out, 'utf8');
+    }
+  }
+}
+
+build().catch(err => {
+  console.error(err);
+  process.exit(1);
+});
